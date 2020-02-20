3c8e20ff3483bff8bfd0058aa9bb060dbdadd394
mint7777 <hanmin84200@gmail.com>
2020-02-20 23:21:32 +0900

    curry 추가

--- index.js
+++ index.js
@@ -31,3 +31,13 @@ function _each(list, iter) {
   }
   return list;
 }
+
+function _curry(fn) {
+  return function(a, b) {
+    return arguments.length === 2
+      ? fn(a, b)
+      : function(b) {
+          return fn(a, b);
+        };
+  };
+}
